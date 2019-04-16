5b14f39f5a52af2ef442089c6cd5ce4847a70b90
Stella Mainar <mainars09@alumni.hanover.edu>
2019-04-16 17:43:17 -0600

    Add encrypt_method to check it can encrypt message with key and date

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,13 @@
 class Enigma
-  
+  def encrypt(message, key, date)
+    shifts = Shift.new(Key.new(key), Offset.new(date))
+
+    {
+      encryption: shifts.get_encryption(message),
+      key: shifts.key.key,
+      date: shifts.offset.date
+    }
+
+  end
+
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -9,4 +9,14 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_it_can_encrypt_a_message_with_a_key_and_date
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+    }
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
 end
