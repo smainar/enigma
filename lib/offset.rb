695b618b9a7bc83e4f0766633d696f798ea85f6f
Stella Mainar <mainars09@alumni.hanover.edu>
2019-04-16 19:02:42 -0600

    Set date argument to nil when initializing Offset class

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,14 +1,14 @@
 class Offset
   attr_reader :date
 
-  def initialize(date)
-    @date = date
+  def initialize(date = nil)
+    @date = dateª
   end
 
   def todays_date
     Date.today.strftime('%d%m%y')
   end
-
+()
   def offset_from_date
     (@date.to_i ** 2).to_s
   end
