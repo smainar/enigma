554ee0940e0522f0f1f909873cdbe5a7c84988c8
Stella Mainar <mainars09@alumni.hanover.edu>
2019-04-16 19:19:38 -0600

    Add test_it_can_encrypt_a_message_and_generates_random_key_and_uses_todays_date to check it outputs correctly

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,9 +1,10 @@
 class Enigma
   def initialize
     @date = Offset.new.todays_date
+    @key = Key.new.random_key
   end
 
-  def encrypt(message, key = @key.random_key, date = @date)
+  def encrypt(message, key = @key, date = @date)
     shifts = Shift.new(Key.new(key), Offset.new(date))
     {
       encryption: shifts.get_encryption(message),
@@ -12,14 +13,13 @@ class Enigma
     }
   end
 
-  def decrypt(message, key, date = @date)
+  def decrypt(message, key = @key, date = @date)
     shifts = Shift.new(Key.new(key), Offset.new(date))
     {
       decryption: shifts.get_decryption(message),
       key: shifts.key.key,
       date: shifts.offset.date
     }
-
   end
 
 end
